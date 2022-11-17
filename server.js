836051a41ed90af5faf78882c23245e8db5a21a2
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-11-17 10:26:24 -0800

    Added tweet endpoints

--- server.js
+++ server.js
@@ -148,12 +148,40 @@ app.post('/users/login', async (req, res) => {
 app.post('/tweet', async (req, res) => {
   console.log('got tweeted');
   try {
-    // const newTweet = await new User({
-    //   tweet: req.body.tweet,
-    // });
-    // const savedTweet = await newTweet.save();
-    // res.status(200).json(savedTweet);
+    const newTweet = await new Tweet({
+      tweet: req.body.tweet,
+      username: req.body.username,
+    });
+    const savedTweet = await newTweet.save();
+    res.status(200).json(savedTweet);
   } catch (err) {
     console.log(err);
+    res.status(500).send();
+  }
+});
+
+app.delete('/tweet/delete-all', async (req, res) => {
+  try {
+    const response = await Tweet.deleteMany({});
+    console.log(response, 'response from tweet.deletemany');
+    if (response.acknowledged) {
+      res.status(200).send();
+    } else {
+      res.status(400).send();
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+});
+
+app.get('/tweets', async (req, res) => {
+  try {
+    console.log('tweets');
+    const allTweets = await Tweet.find();
+    res.status(200).json(allTweets);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send();
   }
 });
