f44d29ce9ac279b10e923783db0f7de45a5505fa
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-12-02 17:52:34 -0800

    update logs

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   if (!authHeader) {
-    console.log('verification: 401 unauthorized');
+    console.log('verification: 401 unauthorized (no token)');
     return res.sendStatus(401); //unauthorized
   }
   // console.log(authHeader); // Bearer token
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
+      console.log('verification: 403 forbidden (no privilege/expired token)');
       return res.sendStatus(403); //forbidden
     }
     // console.log(decoded, 'this is decoded');
--- routes/verify.js
+++ routes/verify.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/verify-current-user', async (req, res) => {
+  console.log('verify-cuirrent-user endpoint');
+  console.log(req.user);
   const user = await User.find({ username: req.user });
   res.status(200).json(user);
 });
