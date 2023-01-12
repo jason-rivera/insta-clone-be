6f54b26587866a4956c8f779bcdca931bbb5ee42
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-01-12 14:30:43 -0800

    Added update password endpoint

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 router.patch('/update', async (req, res) => {
   try {
@@ -21,6 +22,41 @@ router.patch('/update', async (req, res) => {
     console.log(returnUser);
 
     // res.status(200).json(returnUser);
+    res.status(200).send();
+  } catch (e) {
+    console.error(e);
+    res.status(500).send();
+  }
+});
+
+router.patch('/password/update', async (req, res) => {
+  const user = await User.find({ username: { $in: req.body.username } });
+
+  if (user.length === 0) {
+    res.status(401).send();
+    return;
+  }
+
+  const hashedNewPassword = await bcryptjs.hash(req.body.newPassword, 10);
+
+  try {
+    const match = await bcryptjs.compare(
+      req.body.currentPassword,
+      user[0].password
+    );
+    if (match) {
+      const filter = { username: req.body.username };
+      const update = {
+        password: hashedNewPassword,
+      };
+
+      let response = await User.findOneAndUpdate(filter, update);
+
+      res.status(200).send(response);
+    } else {
+      res.status(401).send({ message: 'Incorrect Password' });
+    }
+
     res.status(200).send();
   } catch (e) {
     console.error(e);
