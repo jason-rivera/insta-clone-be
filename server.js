60401add9a0bef3572dbfb3340e5fd03f2341ce6
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-02-01 14:10:27 -0800

    update cors

--- server.js
+++ server.js
@@ -17,15 +17,7 @@ const jwt = require('jsonwebtoken');
 const API_V1 = '/api/v1';
 
 // handles cors
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
-
+app.use(cors());
 app.options('*', cors()); // include before other routes
 
 // handles json body parsing for post requests
