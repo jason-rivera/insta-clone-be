f9b24e64c3bcbe7a04ea9ccef5b896b7db127599
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-02-03 15:04:00 -0800

    edit

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
+const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config({ path: './.env' });
 const express = require('express');
+const router = express.Router();
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 8080;
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const Tweet = require('./models/tweet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const API_V1 = '/api/v1';
+
+// handles cors
+// app.use(
+//   cors({
+//     // origin: 'http://localhost:3000',
+//     // origin: 'https://jason-rivera.github.io',
+//     // origin: 'http://thephotobiography.com',
+//     // origin: 'https://insta-clone-fe.herokuapp.com',
+//     origin: '*',
+//     // methods: ['GET', 'POST'],
+//     // allowedHeaders: ['Content-Type', 'Authorization'],
+//     // credentials: true,
+//   })
+// );
+
+// app.options('*', cors()); // include before other routes
 
 // handles json body parsing for post requests
 app.use(bodyParser.json());
@@ -13,10 +41,33 @@ app.use(express.json());
 // let's you use the cookieParser in your application
 app.use(cookieParser());
 
-app.get('/ping', async (req, res) => {
+// Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+//   .then((result) => {
+//     console.log('Connected to DB');
+//     app.listen(PORT, () => {
+//       console.log(`Listening at: http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((err) => console.log(err));
+
+app.get(API_V1 + '/ping', async (req, res) => {
   res.status(200).send({ message: 'pong' });
 });
 
-app.listen(PORT, () => {
-  console.log('Server listening on http://localhost:' + PORT);
-});
+// These routes won't use verifyJWT middleware
+app.use(API_V1 + '/register', require('./routes/register'));
+app.use(API_V1 + '/auth', require('./routes/auth'));
+
+//-----------------------
+app.use(verifyJWT); // anything below this will use verifyJWT middleware
+//-----------------------
+
+app.use(API_V1 + '/users', require('./routes/users'));
+app.use(API_V1 + '/tweets', require('./routes/tweets'));
+app.use(API_V1 + '/profile', require('./routes/profile'));
+app.use(API_V1 + '/verify', require('./routes/verify'));
